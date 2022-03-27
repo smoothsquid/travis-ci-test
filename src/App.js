b1380fa1d9305df3adb3aa607b50b93db6fc37a9
Jake Lee <jakeogtgg@gmail.com>
2022-03-27 23:08:14 +0900

    미터 인치 계산 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const conversionData = {
+    meter: 1,
+    inch: 39.37,
+    centimeter: 100,
+};
+
+function convert(input, unit, targetUnit) {
+    const conversion = conversionData[targetUnit] / conversionData[unit];
+
+    return input * conversion;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputUnit, setInputUnit] = useState("meter");
+    const [input, setInput] = useState(0);
+    const [targetUnit, setTargetUnit] = useState("inch");
+    const [result, setResult] = useState(0);
+
+    useEffect(() => {
+        setResult(convert(input, inputUnit, targetUnit));
+    }, [input, inputUnit, targetUnit]);
+
+    return (
+        <div>
+            <select
+                value={inputUnit}
+                onChange={({ target }) => setInputUnit(target.value)}
+            >
+                <option value="meter">Meter</option>
+                <option value="inch">Inch</option>
+            </select>
+            <input
+                id="meter"
+                type="number"
+                value={input}
+                onChange={({ target }) => {
+                    setInput(target.value);
+                }}
+                placeholder="Meter"
+            />
+            {"=>"}
+            <select
+                value={targetUnit}
+                onChange={({ target }) => setTargetUnit(target.value)}
+            >
+                <option value="meter">Meter</option>
+                <option value="inch">Inch</option>
+            </select>
+            <label htmlFor="inch">Inch</label>
+            <input
+                id="inch"
+                type="number"
+                value={result}
+                disabled
+                placeholder="Inch"
+            />
+        </div>
+    );
 }
 
 export default App;
